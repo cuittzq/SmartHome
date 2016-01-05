c65a120b02532bbe69eb038f39c7df10f43db709
cuittzq <455164259@qq.com>
2016-01-05 12:16:07 +0800

    添加web api 接口

---
+++ ControlMyPC/ControlMyPC.WebApiUI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ControlMyPC.WebApiUI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ControlMyPC/ControlMyPC.WebApiUI/Controllers/WebApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ControlMyPC.WebApiUI.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        /// <summary>
+        /// 去哪订单处理
+        /// </summary>
+        /// <param name="data">传入参数</param>
+        /// <returns>返回结果</returns>
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        public string OrderHandler([FromBody]object dataObject)
+        {
+            string result = string.Empty;
+            return result;
+        }
+    }
+}
